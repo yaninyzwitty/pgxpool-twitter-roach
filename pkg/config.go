89a63693fbed0e03c9bbcf52a6732946d72ca4bb
yaninyzwitty <ianmwa143@gmail.com>
2025-05-30 22:25:41 +0300

    add api gateway (gqlgen), social service (grpc)

---
+++ pkg/config.go
@@ -0,0 +1,43 @@
+package pkg
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Server   Server   `yaml:"server"`
+	Database Database `yaml:"database"`
+}
+
+type Server struct {
+	Port int `yaml:"port"`
+}
+
+type Database struct {
+	Username   string `yaml:"username"`
+	Host       string `yaml:"host"`
+	DbName     string `yaml:"database"`
+	Port       int    `yaml:"port"`
+	SslMode    string `yaml:"sslmode"`
+	MaxRetries int    `yaml:"max_retries"`
+}
+
+func (c *Config) LoadConfig(filePath string) error {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		slog.Error("Failed to read config file", "file", filePath, "error", err)
+		return fmt.Errorf("failed to read file: %w", err)
+	}
+
+	err = yaml.Unmarshal(data, c)
+	if err != nil {
+		slog.Error("Failed to unmarshal config YAML", "error", err)
+		return fmt.Errorf("failed to unmarshal config: %w", err)
+	}
+
+	return nil
+}
